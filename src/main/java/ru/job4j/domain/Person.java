a2fb061b9a4e5f56776d74dc30315889a62c0ef1
krutaxe <krutaxe@mail.ru>
2023-01-14 22:36:06 +0300

    1. RESTFull. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/domain/Person.java
@@ -0,0 +1,21 @@
+package ru.job4j.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String login;
+    private String password;
+}
