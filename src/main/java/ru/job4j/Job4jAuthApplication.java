aa29784a041d8d5e57cf088bf6388703822db34f
krutaxe <krutaxe@mail.ru>
2023-01-18 07:03:03 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -1,12 +1,30 @@
 package ru.job4j;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class Job4jAuthApplication {
+public class Job4jAuthApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Job4jAuthApplication.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(dataSource);
+        return  liquibase;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
